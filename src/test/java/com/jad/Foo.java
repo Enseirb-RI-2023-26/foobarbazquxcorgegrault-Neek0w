d72730e3845e98a2a022ad64a9557ffdd231a142
neek0w <nicolaseg2003@gmail.com>
2025-01-26 19:22:31 +0100

    init

---
+++ src/test/java/com/jad/Foo.java
@@ -0,0 +1,24 @@
+package com.jad;
+
+import java.util.ArrayList;
+
+public class Foo {
+    private Bar bar;
+    private ArrayList<Baz> bazs = new ArrayList<>();
+    private Qux qux;
+    private Corge corge;
+    private ArrayList<Grault> graults = new ArrayList<>();
+
+
+    public Foo(Bar bar) {
+
+    }
+
+    public void addBaz(Baz baz) {
+
+    }
+
+    public void addGrault() {
+
+    }
+}
---
+++ src/test/java/com/jad/Grault.java
@@ -0,0 +1,10 @@
+package com.jad;
+
+public class Grault {
+    private Foo foo;
+
+
+    public Grault(Foo foo) {
+        this.foo = foo;
+    }
+}
