0565f7bcffd8ad72fba1a0e969668adfe3f24d22
neek0w <nicolaseg2003@gmail.com>
2025-01-26 21:23:50 +0100

    fix Foo

--- src/test/java/com/jad/Foo.java
+++ src/test/java/com/jad/Foo.java
@@ -4,22 +4,50 @@ import java.util.ArrayList;
 
 public class Foo {
     private Bar bar;
-    private ArrayList<Baz> bazs = new ArrayList<>();
+    private ArrayList<Baz> bazs;
     private Qux qux;
     private Corge corge;
-    private ArrayList<Grault> graults = new ArrayList<>();
+    private ArrayList<Grault> graults;
 
 
     public Foo(Bar bar) {
-        this.graults.add(new Grault(this));
+        this.bar = bar;
+        this.bazs = new ArrayList<>();
+        this.graults = new ArrayList<>();
+        this.qux = new Qux();
+    }
+
+    public Bar getbar() {
+        return this.bar;
+    }
+
+    public ArrayList<Baz> getBazs() {
+        return this.bazs;
     }
 
     public void addBaz(Baz baz) {
-        this.bazs.add(new Baz());
+        this.bazs.add(baz);
+    }
+
+    public Qux getQux() {
+        return this.qux;
+    }
+
+    public Corge getCorge() {
+        return this.corge;
+    }
+
+    public void setCorge(Corge corge) {
+        this.corge = corge;
+    }
+
+    public ArrayList<Grault> getGrault() {
+        return this.graults;
     }
 
     public void addGrault() {
         Grault newGrault = new Grault(this);
         this.graults.add(newGrault);
     }
+
 }
--- src/test/java/com/jad/Qux.java
+++ src/test/java/com/jad/Qux.java
@@ -1,5 +1,7 @@
 package com.jad;
 
 public class Qux {
-    
+
+    public void Qux() {
+    }
 }
