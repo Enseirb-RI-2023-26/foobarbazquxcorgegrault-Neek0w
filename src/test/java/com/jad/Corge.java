4b4f9dbed8c1f603ed8f5e5748ca2d1081bfda63
neek0w <nicolaseg2003@gmail.com>
2025-01-26 22:32:35 +0100

    GPT ALED

--- src/test/java/com/jad/Corge.java
+++ src/test/java/com/jad/Corge.java
@@ -1,17 +1,38 @@
 package com.jad;
 
 public class Corge {
-    private Foo foo;
+    private Foo foo; // Peut être null
 
     public Corge(Foo foo) {
         this.foo = foo;
+
+        // Si un Foo est fourni, s'assurer que l'association est correcte
+        if (foo != null && foo.getCorge() != this) {
+            foo.setCorge(this);
+        }
     }
 
     public Foo getFoo() {
         return this.foo;
     }
 
-    public void setFoo(Foo foo) {
-        this.foo = foo;
+    public void setFoo(Foo newFoo) {
+        // Si le Foo est déjà associé, ne rien faire
+        if (this.foo == newFoo) {
+            return;
+        }
+
+        // Dissocier l'ancien Foo
+        Foo oldFoo = this.foo;
+        this.foo = null; // Temporarily nullify to prevent recursion
+        if (oldFoo != null) {
+            oldFoo.setCorge(null);
+        }
+
+        // Associer le nouveau Foo
+        this.foo = newFoo;
+        if (newFoo != null && newFoo.getCorge() != this) {
+            newFoo.setCorge(this);
+        }
     }
 }
--- src/test/java/com/jad/Foo.java
+++ src/test/java/com/jad/Foo.java
@@ -6,16 +6,24 @@ public class Foo {
     private Bar bar;
     private ArrayList<Baz> bazs;
     private Qux qux;
-    private Corge corge;
+    private Corge corge; // Toujours non null
     private ArrayList<Grault> graults;
 
-
-    public Foo(Bar bar) {
+    // Constructeur
+    public Foo(Bar bar, Corge corge) {
+        if (corge == null) {
+            throw new IllegalArgumentException("Corge ne peut pas être null pour Foo.");
+        }
         this.bar = bar;
         this.bazs = new ArrayList<>();
         this.graults = new ArrayList<>();
         this.qux = new Qux();
-        this.corge = null;
+        this.corge = corge;
+
+        // Associer le Foo au Corge
+        if (corge.getFoo() != this) {
+            corge.setFoo(this);
+        }
     }
 
     public Bar getBar() {
@@ -38,8 +46,28 @@ public class Foo {
         return this.corge;
     }
 
-    public void setCorge(Corge corge) {
-        this.corge = corge;
+    public void setCorge(Corge newCorge) {
+        if (newCorge == null) {
+            throw new IllegalArgumentException("Corge ne peut pas être null pour Foo.");
+        }
+
+        // Si le Corge est déjà associé, rien à faire
+        if (this.corge == newCorge) {
+            return;
+        }
+
+        // Dissocier l'ancien Corge
+        Corge oldCorge = this.corge;
+        this.corge = null; // Temporarily nullify to prevent recursion
+        if (oldCorge != null) {
+            oldCorge.setFoo(null);
+        }
+
+        // Associer le nouveau Corge
+        this.corge = newCorge;
+        if (newCorge.getFoo() != this) {
+            newCorge.setFoo(this);
+        }
     }
 
     public ArrayList<Grault> getGrault() {
@@ -50,5 +78,4 @@ public class Foo {
         Grault newGrault = new Grault(this);
         this.graults.add(newGrault);
     }
-
 }
